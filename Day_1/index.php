42b9e78a976a1dc322289cf1c568e293861e94e9
Bobby <bbdgtl@gmail.com>
2019-06-25 09:17:47 +0200

    First commit

---
+++ Day_1/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>My Root Exercises Folder</title>
+</head>
+<body>
+
+  <?php 
+    echo '<h1>This is my parent Exercises folder'
+  ?>
+  
+</body>
+</html>
